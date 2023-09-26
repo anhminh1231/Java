52acfb1148321b9693a1a4735b61d2126a841c2c
anhminh1231 <anhminhne1234@gmail.com>
2023-09-26 20:57:54 +0700

    new

--- Java/Ham/Ham8.java
+++ Java/Ham/Ham8.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class Ham8 {
    public static void main(String[] args) {
         int n;
+        char demn = 'n';
+        char dema = 'a';
+        char demm = 'm';
         
+
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Nhap chuoi n: ");
+            n = Input.nextInt();
+
+        String[] arr = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nhap chuoi " + i + " : ");
+            arr[i] = Input.nextLine();
+        }
+
+        for (int i = 0; i < n; i++) {
+            
+                System.out.print(arr[i] + " ");
+               
+            
+                
+            
+        }
    } 
 }
