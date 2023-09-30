e87031839b0561c89e82e8e2a4343f8376457f68
anhminh1231 <anhminhne1234@gmail.com>
2023-09-30 22:05:20 +0700

    fix

--- Java/Ham/Ham7.java
+++ Java/Ham/Ham7.java
@@ -8,25 +8,34 @@ package Ham;
 import java.util.Scanner;
 
 public class Ham7 {
+
+   public static int Doixung(String x) {
+      int count = 0;
+
+      for (int i = 0; i < x.length() / 2; i++) {
+         if (x.charAt(i) != x.charAt(x.length()-i-1)) {
+            count++;
+         }
+      }
+      return count;
+      //if  count = 0: doi xung 
+      //if count > 0: ko doi xung
+      
+   }
    public static void main(String[] args) {
       String s;
-      int count = 0;
+      
 
       Scanner taochuoi = new Scanner(System.in);
       System.out.print("nhap chuoi cho s: ");
          s = taochuoi.nextLine();
 
-      for (int i = 0; i < s.length() / 2; i++) {
-         if (s.charAt(i) != s.charAt(s.length()-i-1)) {
-            count++;
-         }
-      }
-      if (count > 0 ) {
-         System.out.print("khong phai chuoi doi xung");
+      if (Doixung(s) == 0) {
+         System.out.print("doi xung");
       }
       else {
-         System.out.print("chuoi doi xung");
-      }    
+         System.out.print("khong doi xung");
+      }
 
    } 
 }
--- Java/Ham/Ham8.java
+++ Java/Ham/Ham8.java
@@ -1,17 +1,45 @@
+// Nhập vào mảng có n phần tử chuỗi
+// mỗi chuỗi là 1 tên người
+// Nhập vào tên một người
+// Tìm tất cả các phần tử trong mảng, ai có cùng tên thì nhập vào thì xuất ra màn hình
+// ví dụ: nhập tên Nam
+// thì Nguyễn Thành Nam sẽ được xuất ra màn hình
+// Nguyễn Nam Anh thì không xuất
+
+
 package Ham;
 
 import java.util.Scanner;
 
 public class Ham8 {
-   public static void main(String[] args) {
+    public static String Layten(String x) {
+        //nguyen anh minh
+        if (x.equals("")) {
+            return "";
+        }
+
+        int position = 0;
+        String str;
+
+        for (int a = x.length()-1; a > 0; a--) {
+            if (x.charAt(a) == 32) {
+                position = a;
+                break;
+            }
+        }
+        
+        str = x.substring(position + 1);
+        
+        return str;
+    }
+
+    public static void main(String[] args) {
         int n;
-        char demn = 'n';
-        char dema = 'a';
-        char demm = 'm';
+        String Ten = "";
         
 
         Scanner Input = new Scanner(System.in);
-        System.out.print("Nhap chuoi n: ");
+        System.out.print("Nhap so phan tu cua mang: ");
             n = Input.nextInt();
 
         String[] arr = new String[n];
@@ -21,13 +49,16 @@ public class Ham8 {
             arr[i] = Input.nextLine();
         }
 
+        System.out.print("Nhap Ten: ");
+            Ten = Input.nextLine();
+
+
         for (int i = 0; i < n; i++) {
-            
-                System.out.print(arr[i] + " ");
-               
-            
-                
-            
+            if (Ten.equals(Layten(arr[i]))) {
+                System.out.println("phan tu thu " + i + " " + arr[i]);
+            }
         }
-   } 
+        
+        
+    } 
 }
